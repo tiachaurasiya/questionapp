cb5acd44666b9a3c6fbf27502d940e82a3303d60
Tia Chaurasiya <tiachh73@gmail.com>
2024-06-26 22:52:31 -0400

    Initial Commit

---
+++ questionapp/questionappApp.swift
@@ -0,0 +1,17 @@
+//
+//  questionappApp.swift
+//  questionapp
+//
+//  Created by tia on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct questionappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
